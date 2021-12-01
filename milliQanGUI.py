4f28917624e229ad3892e10c7bb6e6e64a7d7a2b
Xiang Zhang <mr-right@XiangdeMacBook-Pro.local>
2021-12-01 11:17:27 -0500

    new change

--- milliQanGUI.py
+++ milliQanGUI.py
@@ -65,6 +65,8 @@ class worker(QObject):
         p3.terminate()
 
         self.finished.emit()
+        
+
     '''
     def readfile(self):
         self.QTE = QTextEdit(self)
@@ -85,11 +87,11 @@ class gui(QMainWindow):
         self.contents = []
         self.content_temp = []
         #set name and size
-        self.setWindowTitle("DAQCommand Window")
+        #self.setWindowTitle("DAQCommand Window")
         self.title = 'DAQCommand GUI'
         self.left = 10
         self.top = 10
-        self.width = 600
+        self.width = 1000
         self.height = 800
         self.initUI()
         self.onlyfile = []
@@ -105,37 +107,79 @@ class gui(QMainWindow):
         self.SetCombolistButton()
         self.SetTextEdit()
         self.SetqTimer()
-    
+        self.Setimage()
+        self.SetHelpButton()
+        
         self.show()
     
         
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        
+        if False and sys.platform.startswith("darwin"):
+            QApplication.setStyle(QStyleFactory.create('macintosh'))
+            QApplication.setPalette(QApplication.style().standardPalette())
+        elif sys.platform.startswith("linux") or sys.platform.startswith("win") or sys.platform.startswith("darwin"):
+            darkPalette = QPalette()
+            darkPalette.setColor(QPalette.Window, QColor(53,53,53))
+            darkPalette.setColor(QPalette.WindowText, Qt.white)
+            darkPalette.setColor(QPalette.Base, QColor(25,25,25))
+            darkPalette.setColor(QPalette.AlternateBase, QColor(53,53,53))
+            darkPalette.setColor(QPalette.ToolTipBase, Qt.darkGray)
+            darkPalette.setColor(QPalette.ToolTipText, Qt.white)
+            darkPalette.setColor(QPalette.Text, Qt.white)
+            darkPalette.setColor(QPalette.Button, QColor(53,53,53))
+            darkPalette.setColor(QPalette.ButtonText, Qt.white)
+            darkPalette.setColor(QPalette.BrightText, Qt.red)
+            darkPalette.setColor(QPalette.Link, QColor(42, 130, 218))
+            darkPalette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+            darkPalette.setColor(QPalette.HighlightedText, Qt.black)
+            darkPalette.setColor(QPalette.Disabled, QPalette.Window, Qt.lightGray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.WindowText, Qt.gray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.Base, Qt.darkGray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.ToolTipBase, Qt.darkGray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.ToolTipText, Qt.white)
+            darkPalette.setColor(QPalette.Disabled, QPalette.Text, Qt.gray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.Button, QColor(73,73,73))
+            darkPalette.setColor(QPalette.Disabled, QPalette.ButtonText, Qt.lightGray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.BrightText, Qt.lightGray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.Highlight, Qt.lightGray)
+            darkPalette.setColor(QPalette.Disabled, QPalette.HighlightedText, Qt.gray)
+        #self.setStyleSheet("background-color: Dark grey;")
+            QApplication.setStyle(QStyleFactory.create('Fusion'))
+            QApplication.setPalette(darkPalette)
+            
+    def Setimage(self):
+        self.label = QLabel(self)
+        self.pixmap = QPixmap('/Users/mr-right/physics/research2/MilliQanGUI/MicrosoftTeams-image.png')
+        self.label.setPixmap(self.pixmap.scaled(310,120))
+        self.label.move(50,10)
+        self.label.adjustSize()
+        #self.resize(self.pixmap.width(), self.pixmap.height())
         
     def Setlabel(self):
         self.label = QLabel(self)
-        self.label.setText("Button to operate the DAQCommand")
+        self.label.setText("MilliQan DAQCommand")
+        #self.label.setStyleSheet("border: 1px solid black;")
+        self.label.setFont(QFont('Arial',30))
+        self.label.move(400,50)
         self.label.adjustSize()
-        self.label.move(200,50)
-        
     
         
     def SetComboPrint(self):
         self.comboprint = QComboBox(self)
-        self.comboprint.move(350,150)
+        self.comboprint.move(450,150)
         self.comboprint.addItems(["Configure", "Board", "Rate", "Status"])
         
     # To connect a combo box use: combo.activated[str].connect(self.onChanged)
     def SetCombolistButton(self):
         self.printlistbtn = QPushButton("reconfigure", self)
-        self.printlistbtn.move(450,250)
+        self.printlistbtn.move(550,250)
         self.printlistbtn.clicked.connect(self.on_clicled)
         
     def SetCombolist(self):
         self.combolist = QComboBox(self)
-        self.combolist.move(350,250)
+        self.combolist.move(450,250)
         self.combolist.addItems([])
         #self.combolist.currentTextChanged.connect(self.updateCombo)
         #self.combolist.currentTextChanged.connect(self.updateCombo)
@@ -158,40 +202,45 @@ class gui(QMainWindow):
         self.combolist.clear()
         self.combolist.addItems(self.onlyfile)
         
+    def SetHelpButton(self):
+        self.helpbtn = QPushButton("help",self)
+        self.helpbtn.move(650, 250)
+        self.helpbtn.clicked.connect(self.massage)
         
     def SetStartButton(self):
         self.startbtn = QPushButton("start",self)
-        self.startbtn.move(50,150)
+        self.startbtn.move(150,150)
         self.startbtn.clicked.connect(self.longrun_start)
         
         
     def SetStopButton(self):
         self.stopbtn = QPushButton("Stop",self)
-        self.stopbtn.move(150,150)
+        self.stopbtn.move(250,150)
         self.stopbtn.clicked.connect(self.longrun_stop)
     
     
     
     def SetStatusButton(self):
         self.statusbtn = QPushButton("status",self)
-        self.statusbtn.move(250,150)
+        self.statusbtn.move(350,150)
         self.statusbtn.clicked.connect(self.longrun_status)
         
     def SetPrintButton(self):
         self.printbtn = QPushButton("print", self)
-        self.printbtn.move(450,150)
+        self.printbtn.move(550,150)
         self.printbtn.clicked.connect(self.longrun_print)
     
     def SetListButton(self):
         self.listbtn = QPushButton("list",self)
-        self.listbtn.move(150,250)
+        self.listbtn.move(250,250)
         self.listbtn.clicked.connect(self.clicked_list)
         
     def SetTextEdit(self):
         self.QTE = QTextEdit(self)
-        self.QTE.move(50,350)
-        self.QTE.resize(500,300)
+        self.QTE.move(150,350)
+        self.QTE.resize(700,400)
         with open('/var/log/MilliDAQ.log') as f :
+        #with open('/Users/mr-right/physics/research2/textexample.log') as f :
             self.contents = f.readlines()
         #self.QTE.setObjectName("status information")
         #self.QTE.setPlainText(self.contents)
@@ -207,6 +256,7 @@ class gui(QMainWindow):
         self.content_temp = self.contents
         linenumber = len(self.content_temp)
         with open('/var/log/MilliDAQ.log') as f :
+        #with open('/Users/mr-right/physics/research2/textexample.log') as f :
             self.contents = f.readlines()
 
         if self.contents != self.content_temp:
@@ -214,7 +264,13 @@ class gui(QMainWindow):
                 self.QTE.append(i)
                 QCoreApplication.processEvents()
 
+    def massage(self):
+        self.msg = QMessageBox(self)
+        #self.msg.setIcon(QMessageBox.Information)
+        self.msg.setText("DAQCommand: \nstart                  -- start the run\n\nstop                   -- stop the run\n\nprint [configuration | board | rates | status]: \nprint configure  -- print the current V1743Configuration parameters: trigger mode, thresholds, etc\n\nprint board      -- print information about the V1743 board: connection, firmware versions, etc\n\nprint rates       -- print DQM information: trigger rates, missed triggers, etc\n\nprint status     -- print DAQ information: state, configuration path, etc\n\nreconfigure <file.xml> -- stop the run, read/apply a new configuration file, then start\n\nYou can use combo box to choose the information you want to print out. \nYou can use the reconfigure button to reconfigure the file we generate. \nIn the end text box will udapate the information about detector in real time")
         
+        self.msg.setWindowTitle("This is the help window")
+        self.retval = self.msg.exec_()
 
         
     def longrun_start(self):
